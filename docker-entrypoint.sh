9535ce3a37211abbeb87ab1a4d58c44a1fad5150
Gabriel Job <gabrielpjob2002@gmail.com>
2026-05-10 22:01:04 -0300

    fix: entry points

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /var/www/html
 
+echo "==> Regenerando autoloader..."
+composer dump-autoload --no-dev --optimize
+
 echo "==> Limpando caches..."
 php artisan config:clear
 php artisan route:clear
@@ -11,7 +14,7 @@ echo "==> Executando migrations..."
 php artisan migrate --force
 
 echo "==> Executando seeders..."
-php artisan db:seed --class=DatabaseSeeder --force
+php artisan db:seed --force
 
 echo "==> Otimizando para produção..."
 php artisan config:cache
